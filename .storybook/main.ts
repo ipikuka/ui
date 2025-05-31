03dd45da009f9f768f77fc5a306f5e5a203fd53b
talatkuyuk <talatkuyuk@gmail.com>
2025-06-01 00:13:24 +0300

    Chore: prettify .storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,11 +1,18 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: ["@storybook/addon-onboarding", "@storybook/addon-docs"],
+  stories: [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-docs"
+  ],
   framework: {
     name: "@storybook/react-vite",
     options: {},
   },
 };
+
 export default config;
